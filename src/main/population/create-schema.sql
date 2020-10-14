25d32c84c7e14a27c452b3b27c407dfe77083d14
Jaime <jailucloz@alum.us.es>
2020-10-14 19:50:36 +0200

    Terminado level C

--- src/main/population/create-schema.sql
+++ src/main/population/create-schema.sql
@@ -38,6 +38,17 @@
         primary key (`id`)
     ) engine=InnoDB;
 
+    create table `buyer` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `credit_card` varchar(255),
+        `delivery_addres` varchar(255),
+        `email` varchar(255),
+        `phone_number` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
     create table `consumer` (
        `id` integer not null,
         `version` integer not null,
@@ -68,6 +79,22 @@
         `min_money_amount` double precision,
         `min_money_currency` varchar(255),
         `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `item` (
+       `id` integer not null,
+        `version` integer not null,
+        `additional_information` varchar(255),
+        `creation_moment` datetime(6),
+        `description_text` varchar(255),
+        `item_category` varchar(255),
+        `photo` varchar(255),
+        `price_amount` double precision,
+        `price_currency` varchar(255),
+        `ticker` varchar(255),
+        `title` varchar(255),
+        `supplier_id` integer not null,
         primary key (`id`)
     ) engine=InnoDB;
 
@@ -112,6 +139,30 @@
         primary key (`id`)
     ) engine=InnoDB;
 
+    create table `request_item` (
+       `id` integer not null,
+        `version` integer not null,
+        `creation_moment` datetime(6),
+        `notes` varchar(255),
+        `quantity` integer,
+        `ticker` varchar(255),
+        `buyer_id` integer not null,
+        `item_id` integer not null,
+        `supplier_id` integer not null,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `sheet` (
+       `id` integer not null,
+        `version` integer not null,
+        `description` varchar(255),
+        `indexer` integer,
+        `photo` varchar(255),
+        `title` varchar(255),
+        `item_id` integer not null,
+        primary key (`id`)
+    ) engine=InnoDB;
+
     create table `suggestion` (
        `id` integer not null,
         `version` integer not null,
@@ -119,6 +170,17 @@
         `description` varchar(255),
         `email` varchar(255),
         `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `supplier` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `description` varchar(255),
+        `home_page` varchar(255),
+        `item_category` varchar(255),
         primary key (`id`)
     ) engine=InnoDB;
 
@@ -151,6 +213,15 @@
 
     insert into `hibernate_sequence` values ( 1 );
 
+    alter table `item` 
+       add constraint UK_d60jfv0vlrqswikfeec1le23u unique (`ticker`);
+
+    alter table `request_item` 
+       add constraint UK_4id455fdcp9vi9tvbkh9ufbqk unique (`ticker`);
+
+    alter table `sheet` 
+       add constraint UK_thu576g2p11cn0kexvhv3d5k unique (`indexer`);
+
     alter table `user_account` 
        add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
 
@@ -169,12 +240,47 @@
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
 
+    alter table `buyer` 
+       add constraint FK_630a954if6nal5afofvjy73ob 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
     alter table `consumer` 
        add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
 
+    alter table `item` 
+       add constraint `FK7r7pmef5wvaepffbi0xfrso2c` 
+       foreign key (`supplier_id`) 
+       references `supplier` (`id`);
+
     alter table `provider` 
        add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `request_item` 
+       add constraint `FKl95xyup7566n80564w7082s` 
+       foreign key (`buyer_id`) 
+       references `buyer` (`id`);
+
+    alter table `request_item` 
+       add constraint `FKj6yooir0deg6w70lmjtf4pa1k` 
+       foreign key (`item_id`) 
+       references `item` (`id`);
+
+    alter table `request_item` 
+       add constraint `FKc2csi1fre6mjxwc8g5492iqcv` 
+       foreign key (`supplier_id`) 
+       references `supplier` (`id`);
+
+    alter table `sheet` 
+       add constraint `FKot1q9gh25o69kcn1fxi26xi2l` 
+       foreign key (`item_id`) 
+       references `item` (`id`);
+
+    alter table `supplier` 
+       add constraint FK_1h83guf8tf3di74bk4uhuo1ia 
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
--- src/main/population/drop-schema.sql
+++ src/main/population/drop-schema.sql
@@ -11,13 +11,41 @@
        drop 
        foreign key FK_h52w0f3wjoi68b63wv9vwon57;
 
+    alter table `buyer` 
+       drop 
+       foreign key FK_630a954if6nal5afofvjy73ob;
+
     alter table `consumer` 
        drop 
        foreign key FK_6cyha9f1wpj0dpbxrrjddrqed;
 
+    alter table `item` 
+       drop 
+       foreign key `FK7r7pmef5wvaepffbi0xfrso2c`;
+
     alter table `provider` 
        drop 
        foreign key FK_b1gwnjqm6ggy9yuiqm0o4rlmd;
+
+    alter table `request_item` 
+       drop 
+       foreign key `FKl95xyup7566n80564w7082s`;
+
+    alter table `request_item` 
+       drop 
+       foreign key `FKj6yooir0deg6w70lmjtf4pa1k`;
+
+    alter table `request_item` 
+       drop 
+       foreign key `FKc2csi1fre6mjxwc8g5492iqcv`;
+
+    alter table `sheet` 
+       drop 
+       foreign key `FKot1q9gh25o69kcn1fxi26xi2l`;
+
+    alter table `supplier` 
+       drop 
+       foreign key FK_1h83guf8tf3di74bk4uhuo1ia;
 
     drop table if exists `administrator`;
 
@@ -27,11 +55,15 @@
 
     drop table if exists `authenticated`;
 
+    drop table if exists `buyer`;
+
     drop table if exists `consumer`;
 
     drop table if exists `customisation`;
 
     drop table if exists `figment`;
+
+    drop table if exists `item`;
 
     drop table if exists `lucas_bulletin`;
 
@@ -41,7 +73,13 @@
 
     drop table if exists `provider`;
 
+    drop table if exists `request_item`;
+
+    drop table if exists `sheet`;
+
     drop table if exists `suggestion`;
+
+    drop table if exists `supplier`;
 
     drop table if exists `tool_sheet`;
 
